7715b74d4f0de762d491cedda1cda273bf6c70b1
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-02-03 20:15:06 +0530

    Project Init

---
+++ src/main/java/com/shreyash/App.java
@@ -0,0 +1,13 @@
+package com.shreyash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
