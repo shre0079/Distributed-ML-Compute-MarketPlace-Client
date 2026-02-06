9621cf75c66f7b46be3fcf8a93d78471d8afa152
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-02-06 23:44:15 +0530

    worker dto

--- src/main/java/com/client/dto/WorkerInfo.java
+++ src/main/java/com/client/dto/WorkerInfo.java
@@ -1,4 +1,18 @@
 package com.client.dto;
 
 public class WorkerInfo {
+
+    public String workerId;
+    public int cpuCores;
+    public long memoryMB;
+    public String os;
+
+    public WorkerInfo(){}
+
+    public WorkerInfo(String workerId, int cpuCores, long memoryMB, String os) {
+        this.workerId = workerId;
+        this.cpuCores = cpuCores;
+        this.memoryMB = memoryMB;
+        this.os = os;
+    }
 }
