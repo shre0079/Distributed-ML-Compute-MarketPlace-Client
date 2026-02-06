d4ef20eeaaf17d73958432954cea1e18925c5ccc
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-02-06 23:44:35 +0530

    fetch systemInfo and pass to server

--- src/main/java/com/client/util/SystemInfo.java
+++ src/main/java/com/client/util/SystemInfo.java
@@ -1,4 +1,23 @@
 package com.client.util;
 
-public class SystemInfoUtil {
+import com.client.dto.WorkerInfo;
+
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
+import java.util.UUID;
+
+public class SystemInfo {
+    public static WorkerInfo getWorkerInfo(){
+        int cpu=Runtime.getRuntime().availableProcessors();
+
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+        long memoryBytes=osBean.getTotalMemorySize();
+        long memoryMB=memoryBytes / (1024*1024);
+
+        String os=System.getProperty("os.name");
+        String workerId= UUID.randomUUID().toString();
+
+        return  new WorkerInfo(workerId, cpu, memoryMB, os);
+    }
 }
