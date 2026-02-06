3d228ceac8684a4aa532daf7922a8f5f904d1921
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-02-06 23:43:43 +0530

    Docker exec on local system

--- src/main/java/com/client/docker/DockerExecutor.java
+++ src/main/java/com/client/docker/DockerExecutor.java
@@ -1,4 +1,33 @@
 package com.client.docker;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DockerExecutor {
+
+    public static void runContainer(String image) throws Exception{
+
+        ProcessBuilder pb = new ProcessBuilder(
+                "docker",
+                "run",
+                "--rm",
+                image
+        );
+
+        pb.redirectErrorStream(true);
+
+        Process process = pb.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        int exitCode = process.waitFor();
+
+        System.out.println("Container exited with code : " + exitCode);
+    }
 }
