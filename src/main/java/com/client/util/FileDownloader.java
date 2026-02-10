73b75c2a2555606fbc8df4c32d8c3ea4b58d7895
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-02-11 00:34:53 +0530

    file download method

--- src/main/java/com/client/util/FileDownloader.java
+++ src/main/java/com/client/util/FileDownloader.java
@@ -2,4 +2,24 @@ package com.client.util;
 
 public class FileDownloader {
 
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public static Path download(String url, String jobId) throws Exception {
+
+        Path dir = Path.of("jobs",jobId);
+        Files.createDirectories(dir);
+
+        Path filePath = dir.resolve("input.txt");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<InputStream> response = httpClient.send(request,HttpResponse.BodyHandlers.ofInputStream());
+
+        Files.copy(response.body(),filePath);
+
+        return dir;
+    }
 }
