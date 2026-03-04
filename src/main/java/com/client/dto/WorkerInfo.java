2bc600bc00ad21ddc18f9079512794564d37ebd2
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-03-05 00:49:40 +0530

    gpu dto added

--- src/main/java/com/client/dto/WorkerInfo.java
+++ src/main/java/com/client/dto/WorkerInfo.java
@@ -6,13 +6,15 @@ public class WorkerInfo {
     public int cpuCores;
     public long memoryMB;
     public String os;
+    public boolean hasGpu;
 
     public WorkerInfo(){}
 
-    public WorkerInfo(String workerId, int cpuCores, long memoryMB, String os) {
+    public WorkerInfo(String workerId, int cpuCores, long memoryMB, String os,  boolean hasGpu) {
         this.workerId = workerId;
         this.cpuCores = cpuCores;
         this.memoryMB = memoryMB;
         this.os = os;
+        this.hasGpu = hasGpu;
     }
 }
