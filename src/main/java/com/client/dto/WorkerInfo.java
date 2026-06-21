416c560e313a8b08d6e02c673ff74cd123c311aa
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-06-21 14:48:36 +0530

    update: workerInfo dto to include cpu & gpu rate(rent price)

--- src/main/java/com/client/dto/WorkerInfo.java
+++ src/main/java/com/client/dto/WorkerInfo.java
@@ -1,5 +1,7 @@
 package com.client.dto;
 
+import java.math.BigDecimal;
+
 public class WorkerInfo {
 
     public String workerId;
@@ -8,13 +10,18 @@ public class WorkerInfo {
     public String os;
     public boolean hasGpu;
     public String workerSecret;
+    public BigDecimal cpuRatePerSecond;
+    public BigDecimal gpuRatePerSecond;
 
-    public WorkerInfo(String workerId, int cpuCores, long memoryMB, String os, boolean hasGpu, String workerSecret) {
+
+    public WorkerInfo(String workerId, int cpuCores, long memoryMB, String os, boolean hasGpu, String workerSecret, BigDecimal cpuRatePerSecond, BigDecimal gpuRatePerSecond) {
         this.workerId = workerId;
         this.cpuCores = cpuCores;
         this.memoryMB = memoryMB;
         this.os = os;
         this.hasGpu = hasGpu;
         this.workerSecret = workerSecret;
+        this.cpuRatePerSecond = cpuRatePerSecond;
+        this.gpuRatePerSecond = gpuRatePerSecond;
     }
 }
